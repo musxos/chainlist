12f4615d3608a389e55510656b24e6a8007ba0a8
codeturk <34295148+musxos@users.noreply.github.com>
2022-03-21 18:10:35 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ function Home({ changeTheme, theme, sortedChains }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Chainlist</title>
+        <title>AllChain</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -93,7 +93,7 @@ function Home({ changeTheme, theme, sortedChains }) {
           <div className={classes.copyContainer}>
             <div className={classes.copyCentered}>
               <Typography variant="h1" className={classes.chainListSpacing}>
-                <span className={classes.helpingUnderline}>Chainlist</span>
+                <span className={classes.helpingUnderline}>AllChain</span>
               </Typography>
               <Typography variant="h2" className={classes.helpingParagraph}>
                 Helping users connect to EVM powered networks
@@ -125,18 +125,31 @@ function Home({ changeTheme, theme, sortedChains }) {
               <div className={classes.socials}>
                 <a
                   className={`${classes.socialButton}`}
-                  href="https://github.com/DefiLlama/chainlist"
+                  href="https://t.me/RuesandoraChat"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <svg version="1.1" width="24" height="24" viewBox="0 0 24 24">
-                    <path
-                      fill={'#2F80ED'}
-                      d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"
-                    />
+                     <path d="
+                      M 630, 425
+                      A 195, 195 0 0 1 331, 600
+                      A 142, 142 0 0 0 428, 570
+                      A  70,  70 0 0 1 370, 523
+                      A  70,  70 0 0 0 401, 521
+                      A  70,  70 0 0 1 344, 455
+                      A  70,  70 0 0 0 372, 460
+                      A  70,  70 0 0 1 354, 370
+                      A 195, 195 0 0 0 495, 442
+                      A  67,  67 0 0 1 611, 380
+                      A 117, 117 0 0 0 654, 363
+                      A  65,  65 0 0 1 623, 401
+                      A 117, 117 0 0 0 662, 390
+                      A  65,  65 0 0 1 630, 425
+                      Z"
+                      style="fill:#3BA9EE;"/>
                   </svg>
                   <Typography variant="body1" className={classes.sourceCode}>
-                    View Source Code
+                    Follow on Twitter
                   </Typography>
                 </a>
 
@@ -153,7 +166,7 @@ function Home({ changeTheme, theme, sortedChains }) {
                     ></path>
                   </svg>
                   <Typography variant="body1" className={classes.sourceCode}>
-                    Join our Discord
+                    Follow on Youtube
                   </Typography>
                 </a>
               </div>
